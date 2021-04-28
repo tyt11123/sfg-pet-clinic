a2c17f5e9d34595d07a60999a021e88ba2138526
Kam Yiu Cheung <tyt11123@yahoo.com.hk>
2021-04-28 16:32:32 +0800

    Initial Project Add

---
+++ src/test/java/com/bouncingscout/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bouncingscout.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
