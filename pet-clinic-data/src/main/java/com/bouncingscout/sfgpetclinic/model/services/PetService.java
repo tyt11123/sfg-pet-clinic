de8ac304ffbf3daba84a29185466a0601da8a270
Kam Yiu Cheung <tyt11123@yahoo.com.hk>
2021-05-31 11:43:32 +0800

    Implements interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/com/bouncingscout/sfgpetclinic/model/services/PetService.java
@@ -0,0 +1,11 @@
+package com.bouncingscout.sfgpetclinic.model.services;
+
+import com.bouncingscout.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/bouncingscout/sfgpetclinic/model/services/VetService.java
@@ -0,0 +1,11 @@
+package com.bouncingscout.sfgpetclinic.model.services;
+
+import com.bouncingscout.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
